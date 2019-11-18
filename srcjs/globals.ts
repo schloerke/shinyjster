fd79aa5c24c28a55a5bed4992ce937096d78c70c
Barret Schloerke <schloerke@gmail.com>
2019-11-18 12:02:52 -0500

    Fix ts error on Jster globals export

--- srcjs/globals.ts
+++ srcjs/globals.ts
@@ -12,7 +12,7 @@ declare global {
   interface Window {
     Shiny: ShinyType;
     jQuery: JQueryStatic;
-    Jster: Jster;
+    Jster: typeof Jster; // eslint-disable-line no-undef
     jster: (timeout: number) => void;
   }
 }
