26534c7976179b9444dde3de635f6b9e72b848a1
Barret Schloerke <schloerke@gmail.com>
2019-11-18 12:03:53 -0500

    lints

--- srcjs/jster.ts
+++ srcjs/jster.ts
@@ -150,12 +150,13 @@ class Jster {
 
   static getParameterByName(name: string, url: string): string {
     if (!url) url = window.location.href;
-    name = name.replace(/[\\[\\]]/g, '\\\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
+    name = name.replace(/[\\[\\]]/g, "\\\\$&");
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+
     if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\\+/g, ' '));
+    if (!results[2]) return "";
+    return decodeURIComponent(results[2].replace(/\\+/g, " "));
   }
 }
 
