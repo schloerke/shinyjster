c38c5e20613c7b904e21d92ab8495fbc817c3f74
Barret Schloerke <schloerke@gmail.com>
2019-11-19 10:20:41 -0500

    Add 130-output-null app

---
+++ inst/shinyjster/130-output-null/app.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(shinyjster)
+​
+ui <- fluidPage(
+  h3("Pressing \"Clear\" should clear the plot."),
+  plotOutput("plot"),
+  actionButton("clear", "Clear"),
+​
+  shinyjster_js("
+    var jst = jster();
+    jst.add(function(done) { $('#clear').click(); done() });
+    jst.waitForShiny();
+    jst.add(function(done) {
+      if ($('#plot').children().length == 0) done();
+      throw 'Plot is not empty.';
+    });
+    jst.test();
+    "
+  )
+)
+​
+server <- function(input, output, session) {
+  shinyjster_server(input, output, session)
+​
+  output$plot <- renderPlot({ plot(cars) })
+​
+  observeEvent(input$clear, {
+    output$plot <- NULL
+  })
+}
+​
+shinyApp(ui, server)
