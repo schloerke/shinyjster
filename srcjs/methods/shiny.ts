dd5550af93054b62ef336205a49f44f7f5a022b1
Barret Schloerke <schloerke@gmail.com>
2019-11-21 10:58:47 -0500

    Use individual event handlers for Jster.shiny.waitUntilIdle
    * Speed up `waitUntilIdle` to 0 ms
    * Add `waitUntilIdleFor` which requires a delay
    * Add `waitUntilStable` which defaults to `waitUntilIdleFor` with a 200ms delay

--- srcjs/methods/shiny.ts
+++ srcjs/methods/shiny.ts
@@ -2,25 +2,16 @@ import { $ } from "../globals";
 import { debounce } from "lodash";
 
 let shinyIsIdle = false;
-const shinyIdleFns = [];
 
 function isIdle(): boolean {
   return shinyIsIdle;
 }
-function callIdleFns() {
-  // call using setTimeout to "break" serialized execution
-  shinyIdleFns.map(function(fn) {
-    setTimeout(fn, 0);
-  });
+function isBusy(): boolean {
+  return !shinyIsIdle;
 }
-
-// wait 200ms before calling callIdleFns
-const callIdleFnsDebounced = debounce(callIdleFns, 200);
-
 if ($) {
   $(document).on("shiny:busy", function(event) {
     shinyIsIdle = false;
-    callIdleFnsDebounced.cancel();
   });
   $(document).on("shiny:idle", function(event) {
     shinyIsIdle = true;
@@ -30,26 +21,60 @@ if ($) {
     //   if shiny decides to become 'idle', then immediately become 'busy', `waitUntilIdle` should NOT be called.
     // To combat this, debouncing is used to make sure there is a 200ms delay
     //   and _.cancel is used to stop any debounced `waitUntilIdle` call if suddendly shiny is 'busy'.
-    callIdleFnsDebounced();
   });
 }
 
-function waitUntilIdle(callback) {
-  // add to idle queue
-  shinyIdleFns.push(callback);
+// to avoid idle and busy thrashing,
+//   call the debounced form of `callIdleFns`
+// `waitUntilIdle` is interpreted as "Shiny must be in the 'idle' state for at least 200ms"
+//   if shiny decides to become 'idle', then immediately become 'busy', `waitUntilIdle` should NOT be called.
+// To combat this, debouncing is used to make sure there is a 200ms delay
+//   and _.cancel is used to stop any debounced `waitUntilIdle` call if suddendly shiny is 'busy'.
+function waitUntilIdleFor(timeout) {
+  return function(callback) {
+    let timeoutId = null;
+    const randomVal = Math.random();
+    const busyFn = function() {
+      console.log("busy!", randomVal);
+      clearTimeout(timeoutId);
+    };
+    const idleFn = function() {
+      console.log("idle!", randomVal);
+      const fn = function() {
+        console.log("success!", randomVal);
+        // made it through the timeout, remove event listeners
+        $(document).off("shiny:busy", busyFn);
+        $(document).off("shiny:idle", idleFn);
+        callback();
+      };
 
-  if (shinyIsIdle) {
-    // kick it off, if possible
-    callIdleFnsDebounced();
-  }
+      timeoutId = setTimeout(fn, timeout);
+    };
+
+    $(document).on("shiny:busy", busyFn);
+    $(document).on("shiny:idle", idleFn);
+
+    if (shinyIsIdle) {
+      idleFn();
+    }
+  };
 }
-
-function isBusy(): boolean {
-  return !shinyIsIdle;
+function waitUntilIdle(callback) {
+  waitUntilIdleFor(0)(callback);
+}
+function waitUntilStable(callback) {
+  waitUntilIdleFor(200)(callback);
 }
 
 function hasOverlay() {
   return $("#shiny-disconnected-overlay").length > 0;
 }
 
-export { waitUntilIdle, isBusy, hasOverlay, isIdle };
+export {
+  isIdle,
+  isBusy,
+  hasOverlay,
+  waitUntilIdleFor,
+  waitUntilIdle,
+  waitUntilStable,
+};
