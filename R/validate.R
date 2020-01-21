df3bd1a56a3761800d80d58baaa6b21806efa621
Barret Schloerke <schloerke@gmail.com>
2020-01-21 11:48:13 -0500

    require a minimum set of names. Better error format

--- R/validate.R
+++ R/validate.R
@@ -7,14 +7,14 @@
 #' @export
 assert_jster <- function(dt) {
   testthat::expect_s3_class(dt, "data.frame")
-  testthat::expect_named(dt, c("appDir", "successful", "returnValue"))
+  testthat::expect_true(all(c("appDir", "successful", "returnValue") %in% names(dt)))
 
   not_successful <- !dt$successful
 
   if (any(not_successful)) {
     fail_dt <- dt[not_successful, ]
     str(as.list(fail_dt))
-    stop("shinyjster - Failing apps:\n", paste0("shinyjster - * ", fail_dt$appDir, collapse = "\n"))
+    stop("\nshinyjster - Failing apps:\n", paste0("shinyjster - * ", fail_dt$appDir, collapse = "\n"))
   } else {
     message("shinyjster - Success! All apps pass")
   }
